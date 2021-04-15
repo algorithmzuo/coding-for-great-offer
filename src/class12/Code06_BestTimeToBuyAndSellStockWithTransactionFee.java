f5759e39ea29858e68cda18f1a68be8cdeee5727
algorithmzuo <algorithmzuo@163.com>
2021-04-15 17:31:53 +0800

    modify codd

--- src/class12/Code06_BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/class12/Code06_BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -8,17 +8,15 @@ public class Code06_BestTimeToBuyAndSellStockWithTransactionFee {
 			return 0;
 		}
 		int N = arr.length;
-		int ans = 0;
 		int bestbuy = -arr[0] - fee;
 		int bestsell = 0;
 		for (int i = 1; i < N; i++) {
 			int curbuy = bestsell - arr[i] - fee;
 			int cursell = bestbuy + arr[i];
-			ans = Math.max(ans, cursell);
 			bestbuy = Math.max(bestbuy, curbuy);
 			bestsell = Math.max(bestsell, cursell);
 		}
-		return ans;
+		return bestsell;
 	}
 
 }
