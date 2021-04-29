29153d5401d0e8bd036b1e283521e35eebffee68
algorithmzuo <algorithmzuo@163.com>
2021-04-29 15:20:54 +0800

    modify code

--- src/class04/Code05_CandyProblem.java
+++ src/class04/Code05_CandyProblem.java
@@ -3,12 +3,39 @@ package class04;
 // 测试链接 : https://leetcode.com/problems/candy/
 public class Code05_CandyProblem {
 
-	// 这是原问题
+	// 这是原问题的优良解
+	// 时间复杂度O(N)，额外空间复杂度O(N)
 	public static int candy1(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
-		int index = nextMinIndex1(arr, 0);
+		int N = arr.length;
+		int[] left = new int[N];
+		for (int i = 1; i < N; i++) {
+			if (arr[i - 1] < arr[i]) {
+				left[i] = left[i - 1] + 1;
+			}
+		}
+		int[] right = new int[N];
+		for (int i = N - 2; i >= 0; i--) {
+			if (arr[i] > arr[i + 1]) {
+				right[i] = right[i + 1] + 1;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			ans += Math.max(left[i], right[i]);
+		}
+		return ans + N;
+	}
+
+	// 这是原问题空间优化后的解
+	// 时间复杂度O(N)，额外空间复杂度O(1)
+	public static int candy2(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int index = nextMinIndex2(arr, 0);
 		int res = rightCands(arr, 0, index++);
 		int lbase = 1;
 		int next = 0;
@@ -19,7 +46,7 @@ public class Code05_CandyProblem {
 				res += ++lbase;
 				index++;
 			} else if (arr[index] < arr[index - 1]) {
-				next = nextMinIndex1(arr, index - 1);
+				next = nextMinIndex2(arr, index - 1);
 				rcands = rightCands(arr, index - 1, next++);
 				rbase = next - index + 1;
 				res += rcands + (rbase > lbase ? -lbase : -rbase);
@@ -34,7 +61,7 @@ public class Code05_CandyProblem {
 		return res;
 	}
 
-	public static int nextMinIndex1(int[] arr, int start) {
+	public static int nextMinIndex2(int[] arr, int start) {
 		for (int i = start; i != arr.length - 1; i++) {
 			if (arr[i] <= arr[i + 1]) {
 				return i;
@@ -48,12 +75,13 @@ public class Code05_CandyProblem {
 		return n + n * (n - 1) / 2;
 	}
 
-	// 这是进阶问题，不要提交这个
-	public static int candy2(int[] arr) {
+	// 这是进阶问题的最优解，不要提交这个
+	// 时间复杂度O(N), 额外空间复杂度O(1)
+	public static int candy3(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
-		int index = nextMinIndex2(arr, 0);
+		int index = nextMinIndex3(arr, 0);
 		int[] data = rightCandsAndBase(arr, 0, index++);
 		int res = data[0];
 		int lbase = 1;
@@ -65,7 +93,7 @@ public class Code05_CandyProblem {
 				same = 1;
 				index++;
 			} else if (arr[index] < arr[index - 1]) {
-				next = nextMinIndex2(arr, index - 1);
+				next = nextMinIndex3(arr, index - 1);
 				data = rightCandsAndBase(arr, index - 1, next++);
 				if (data[1] <= lbase) {
 					res += data[0] - data[1];
@@ -84,7 +112,7 @@ public class Code05_CandyProblem {
 		return res;
 	}
 
-	public static int nextMinIndex2(int[] arr, int start) {
+	public static int nextMinIndex3(int[] arr, int start) {
 		for (int i = start; i != arr.length - 1; i++) {
 			if (arr[i] < arr[i + 1]) {
 				return i;
@@ -108,10 +136,10 @@ public class Code05_CandyProblem {
 
 	public static void main(String[] args) {
 		int[] test1 = { 3, 0, 5, 5, 4, 4, 0 };
-		System.out.println(candy1(test1));
+		System.out.println(candy2(test1));
 
 		int[] test2 = { 3, 0, 5, 5, 4, 4, 0 };
-		System.out.println(candy2(test2));
+		System.out.println(candy3(test2));
 	}
 
 }
