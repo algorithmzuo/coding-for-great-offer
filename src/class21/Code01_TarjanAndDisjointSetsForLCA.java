c85e743ebceb6b41421ac87cdf5cc833c22de9d7
algorithmzuo <algorithmzuo@163.com>
2021-06-30 19:22:28 +0800

    modify code

--- src/class21/Code01_TarjanAndDisjointSetsForLCA.java
+++ src/class21/Code01_TarjanAndDisjointSetsForLCA.java
@@ -1,266 +1,267 @@
 package class21;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.HashSet;
 
 public class Code01_TarjanAndDisjointSetsForLCA {
 
-	public static class Element<V> {
-		public V value;
+	// tree是以为数组，大小为N，表示一共有N个节点，编号为0~N-1
+	// tree[i] = j 表示点i的父亲是点j，
+	// tree可以保证一定是二叉树或者多叉树，且一定不是森林
+	//
+	// queries是二维数组，大小为M*2，比如
+	// [
+	// [4,9], 想查询4和9之间的最低公共祖先
+	// [3,7], 想查询3和7之间的最低公共祖先
+	// [8,1], 想查询8和1之间的最低公共祖先
+	// ...
+	// ]
+	//
+	// tree数组和queries数组里面的所有值，都一定保证在0~N-1之间
+	// 要求返回一个一维数组，大小为M，表示每一条查询的答案
 
-		public Element(V value) {
-			this.value = value;
-		}
-
-	}
-
-	public static class UnionFindSet<V> {
-		public HashMap<V, Element<V>> elementMap;
-		public HashMap<Element<V>, Element<V>> fatherMap;
-		public HashMap<Element<V>, Integer> sizeMap;
-
-		public UnionFindSet(List<V> list) {
-			elementMap = new HashMap<>();
-			fatherMap = new HashMap<>();
-			sizeMap = new HashMap<>();
-			for (V value : list) {
-				Element<V> element = new Element<V>(value);
-				elementMap.put(value, element);
-				fatherMap.put(element, element);
-				sizeMap.put(element, 1);
+	// 暴力方法
+	public static int[] query1(int[] tree, int[][] queries) {
+		int M = queries.length;
+		int[] ans = new int[M];
+		HashSet<Integer> path = new HashSet<>();
+		for (int i = 0; i < M; i++) {
+			int jump = queries[i][0];
+			while (tree[jump] != jump) {
+				path.add(jump);
+				jump = tree[jump];
 			}
-		}
-
-		private Element<V> findHead(Element<V> element) {
-			Stack<Element<V>> path = new Stack<>();
-			while (element != fatherMap.get(element)) {
-				path.push(element);
-				element = fatherMap.get(element);
+			path.add(jump);
+			jump = queries[i][1];
+			while (!path.contains(jump)) {
+				jump = tree[jump];
 			}
-			while (!path.isEmpty()) {
-				fatherMap.put(path.pop(), element);
-			}
-			return element;
+			ans[i] = jump;
+			path.clear();
 		}
-
-		// 返回node所在集合的代表节点
-		public V findHead(V node) {
-			return elementMap.containsKey(node) ? findHead(elementMap
-					.get(node)).value : null;
-		}
-
-		public boolean isSameSet(V a, V b) {
-			if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
-				return findHead(elementMap.get(a)) == findHead(elementMap
-						.get(b));
-			}
-			return false;
-		}
-
-		public void union(V a, V b) {
-			if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
-				Element<V> aF = findHead(elementMap.get(a));
-				Element<V> bF = findHead(elementMap.get(b));
-				if (aF != bF) {
-					int aFrank = sizeMap.get(aF);
-					int bFrank = sizeMap.get(bF);
-					if (aFrank >= bFrank) {
-						fatherMap.put(bF, aF);
-						sizeMap.put(aF, aFrank + bFrank);
-						sizeMap.remove(bF);
-					} else {
-						fatherMap.put(aF, bF);
-						sizeMap.put(bF, aFrank + bFrank);
-						sizeMap.remove(aF);
-					}
-				}
-			}
-		}
-
-	}
-
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
-
-		public Node(int data) {
-			this.value = data;
-		}
-	}
-
-	public static class Query {
-		public Node o1;
-		public Node o2;
-
-		public Query(Node o1, Node o2) {
-			this.o1 = o1;
-			this.o2 = o2;
-		}
-	}
-
-	public static Node[] tarJanQuery(Node head, Query[] quries) {
-		HashMap<Node, LinkedList<Node>> queryMap = new HashMap<>();
-		HashMap<Node, LinkedList<Integer>> indexMap = new HashMap<>();
-		// key 某个集合的代表节点    value 代表节点所在集合中的所有节点，祖先是谁
-		
-		
-		// key 表示一个集合的代表节点
-		// value  这个key所在集合的所有节点，tag点
-		HashMap<Node, Node> tagMap = new HashMap<>();
-		UnionFindSet<Node> sets = new UnionFindSet<>(getAllNodes(head));
-		Node[] ans = new Node[quries.length];
-		// 提前填好无效问题的解，对于有效问题，生成好queryMap和indexMap
-		setQueriesAndSetEasyAnswers(quries, ans, queryMap, indexMap);
-		
-		// 二叉树头节点head，遍历
-		// 并查集sets，打tag，还有tagMap表
-		// queryMap, indexMap可以方便的知道有哪些问题
-		// ans 知道答案了可以填写
-		// 递归遍历的过程中，把有效问题解决，填到答案里去
-		setAnswers(head, ans, queryMap, indexMap, tagMap, sets);
 		return ans;
 	}
 
-	public static List<Node> getAllNodes(Node head) {
-		List<Node> res = new ArrayList<>();
-		process(head, res);
-		return res;
-	}
-
-	public static void process(Node head, List<Node> res) {
-		if (head == null) {
-			return;
-		}
-		res.add(head);
-		process(head.left, res);
-		process(head.right, res);
-	}
-
-	public static void setQueriesAndSetEasyAnswers(Query[] ques, Node[] ans,
-			HashMap<Node, LinkedList<Node>> queryMap,
-			HashMap<Node, LinkedList<Integer>> indexMap) {
-		Node o1 = null;
-		Node o2 = null;
-		for (int i = 0; i != ans.length; i++) {
-			o1 = ques[i].o1;
-			o2 = ques[i].o2;
-			if (o1 == o2 || o1 == null || o2 == null) {
-				ans[i] = o1 != null ? o1 : o2;
+	// 最优解
+	public static int[] query2(int[] tree, int[][] queries) {
+		int N = tree.length;
+		int M = queries.length;
+		int[] help = new int[N];
+		int h = 0;
+		for (int i = 0; i < N; i++) {
+			if (tree[i] == i) {
+				h = i;
 			} else {
-				if (!queryMap.containsKey(o1)) {
-					queryMap.put(o1, new LinkedList<Node>());
-					indexMap.put(o1, new LinkedList<Integer>());
-				}
-				if (!queryMap.containsKey(o2)) {
-					queryMap.put(o2, new LinkedList<Node>());
-					indexMap.put(o2, new LinkedList<Integer>());
-				}
-				queryMap.get(o1).add(o2);
-				indexMap.get(o1).add(i);
-				queryMap.get(o2).add(o1);
-				indexMap.get(o2).add(i);
+				help[tree[i]]++;
+			}
+		}
+		int[][] mt = new int[N][];
+		for (int i = 0; i < N; i++) {
+			mt[i] = new int[help[i]];
+		}
+		for (int i = 0; i < N; i++) {
+			if (i != h) {
+				mt[tree[i]][--help[tree[i]]] = i;
+			}
+		}
+		for (int i = 0; i < M; i++) {
+			if (queries[i][0] != queries[i][1]) {
+				help[queries[i][0]]++;
+				help[queries[i][1]]++;
+			}
+		}
+		int[][] mq = new int[N][];
+		int[][] mi = new int[N][];
+		for (int i = 0; i < N; i++) {
+			mq[i] = new int[help[i]];
+			mi[i] = new int[help[i]];
+		}
+		for (int i = 0; i < M; i++) {
+			if (queries[i][0] != queries[i][1]) {
+				mq[queries[i][0]][--help[queries[i][0]]] = queries[i][1];
+				mi[queries[i][0]][help[queries[i][0]]] = i;
+				mq[queries[i][1]][--help[queries[i][1]]] = queries[i][0];
+				mi[queries[i][1]][help[queries[i][1]]] = i;
+			}
+		}
+		int[] ans = new int[M];
+		UnionFind uf = new UnionFind(N);
+		process(h, mt, mq, mi, uf, ans);
+		for (int i = 0; i < M; i++) {
+			if (queries[i][0] == queries[i][1]) {
+				ans[i] = queries[i][0];
+			}
+		}
+		return ans;
+	}
+
+	public static void process(int head, int[][] mt, int[][] mq, int[][] mi, UnionFind uf, int[] ans) {
+		for (int next : mt[head]) {
+			process(next, mt, mq, mi, uf, ans);
+			uf.union(head, next);
+			uf.setTag(head, head);
+		}
+		int[] q = mq[head];
+		int[] i = mi[head];
+		for (int k = 0; k < q.length; k++) {
+			int tag = uf.getTag(q[k]);
+			if (tag != -1) {
+				ans[i[k]] = tag;
 			}
 		}
 	}
 
-	public static void setAnswers(
-			Node head, 
-			Node[] ans,
-			HashMap<Node, LinkedList<Node>> queryMap,
-			HashMap<Node, LinkedList<Integer>> indexMap,
-			HashMap<Node, Node> tagMap, 
-			UnionFindSet<Node> sets) {
-		if (head == null) {
-			return;
-		}
-		setAnswers(head.left, ans, queryMap, indexMap, tagMap, sets);
-		sets.union(head.left, head);
-		tagMap.put(sets.findHead(head), head);
-		setAnswers(head.right, ans, queryMap, indexMap, tagMap, sets);
-		sets.union(head.right, head);
-		tagMap.put(sets.findHead(head), head);
-		// 处理有关于head的问题
-		LinkedList<Node> nList = queryMap.get(head);
-		LinkedList<Integer> iList = indexMap.get(head);
-		Node node = null;
-		Node nodeFather = null;
-		int index = 0;
-		// 3   {7,8,9}
-		// 7,8,9
-		// 7
-		while (nList != null && !nList.isEmpty()) {
-			node = nList.poll(); // head  node
-			index = iList.poll(); // index
-			nodeFather = sets.findHead(node); // node在集合中的代表节点
-			if (tagMap.containsKey(nodeFather)) { // node所在的集合是否设置过tag
-				ans[index] = tagMap.get(nodeFather);
+	public static class UnionFind {
+		private int[] f;
+		private int[] s;
+		private int[] t;
+		private int[] h;
+
+		public UnionFind(int N) {
+			f = new int[N];
+			s = new int[N];
+			t = new int[N];
+			h = new int[N];
+			for (int i = 0; i < N; i++) {
+				f[i] = i;
+				s[i] = 1;
+				t[i] = -1;
 			}
 		}
-	}
 
-	// for test -- print tree
-	public static void printTree(Node head) {
-		System.out.println("Binary Tree:");
-		printInOrder(head, 0, "H", 17);
-		System.out.println();
-	}
-
-	public static void printInOrder(Node head, int height, String to, int len) {
-		if (head == null) {
-			return;
+		private int find(int i) {
+			int j = 0;
+			while (i != f[i]) {
+				h[j++] = i;
+				i = f[i];
+			}
+			while (j > 0) {
+				h[--j] = i;
+			}
+			return i;
 		}
-		printInOrder(head.right, height + 1, "v", len);
-		String val = to + head.value + to;
-		int lenM = val.length();
-		int lenL = (len - lenM) / 2;
-		int lenR = len - lenM - lenL;
-		val = getSpace(lenL) + val + getSpace(lenR);
-		System.out.println(getSpace(height * len) + val);
-		printInOrder(head.left, height + 1, "^", len);
-	}
 
-	public static String getSpace(int num) {
-		String space = " ";
-		StringBuffer buf = new StringBuffer("");
-		for (int i = 0; i < num; i++) {
-			buf.append(space);
+		public void union(int i, int j) {
+			int fi = find(i);
+			int fj = find(j);
+			if (fi != fj) {
+				int si = s[fi];
+				int sj = s[fj];
+				int m = si >= sj ? fi : fj;
+				int l = m == fi ? fj : fi;
+				f[l] = m;
+				s[m] += s[l];
+			}
 		}
-		return buf.toString();
+
+		public void setTag(int i, int tag) {
+			t[find(i)] = tag;
+		}
+
+		public int getTag(int i) {
+			return t[find(i)];
+		}
+
 	}
 
+	// 为了测试
+	// 随机生成N个节点树，可能是多叉树，并且一定不是森林
+	// 输入参数N要大于0
+	public static int[] generateTreeArray(int N) {
+		int[] order = new int[N];
+		for (int i = 0; i < N; i++) {
+			order[i] = i;
+		}
+		for (int i = N - 1; i >= 0; i--) {
+			swap(order, i, (int) (Math.random() * (i + 1)));
+		}
+		int[] ans = new int[N];
+		ans[order[0]] = order[0];
+		for (int i = 1; i < N; i++) {
+			ans[order[i]] = order[(int) (Math.random() * i)];
+		}
+		return ans;
+	}
+
+	// 为了测试
+	// 随机生成M条查询，点有N个，点的编号在0~N-1之间
+	// 输入参数M和N都要大于0
+	public static int[][] generateQueries(int M, int N) {
+		int[][] ans = new int[M][2];
+		for (int i = 0; i < M; i++) {
+			ans[i][0] = (int) (Math.random() * N);
+			ans[i][1] = (int) (Math.random() * N);
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	// 为了测试
+	public static boolean equal(int[] a, int[] b) {
+		if (a.length != b.length) {
+			return false;
+		}
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != b[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// 为了测试
 	public static void main(String[] args) {
-		Node head = new Node(1);
-		head.left = new Node(2);
-		head.right = new Node(3);
-		head.left.left = new Node(4);
-		head.left.right = new Node(5);
-		head.right.left = new Node(6);
-		head.right.right = new Node(7);
-		head.right.right.left = new Node(8);
-		printTree(head);
+		int N = 1000;
+		int M = 200;
+		int testTime = 10000;
+		System.out.println("功能测试开始");
+		for (int i = 0; i < testTime; i++) {
+			int size = (int) (Math.random() * N) + 1;
+			int ques = (int) (Math.random() * M) + 1;
+			int[] tree = generateTreeArray(size);
+			int[][] queries = generateQueries(ques, size);
+			int[] ans1 = query1(tree, queries);
+			int[] ans2 = query2(tree, queries);
+			if (!equal(ans1, ans2)) {
+				System.out.println("出错了！");
+			}
+		}
+		System.out.println("功能测试结束");
+
 		System.out.println("===============");
 
-		Query[] qs = new Query[7];
-		qs[0] = new Query(head.left.right, head.right.left);
-		qs[1] = new Query(head.left.left, head.left);
-		qs[2] = new Query(head.right.left, head.right.right.left);
-		qs[3] = new Query(head.left.left, head.right.right);
-		qs[4] = new Query(head.right.right, head.right.right.left);
-		qs[5] = new Query(head, head);
-		qs[6] = new Query(head.left, head.right.right.left);
+		System.out.println("性能测试开始");
+		System.out.println("如果树非常不平衡呈现链状，方法1会特别慢");
+		System.out.println("不过该文件中生成树的方式很难让其变成链状");
+		System.out.println("所以性能差异并不明显");
+		System.out.println("但是在查询条数很多时，可以看到方法2还是比方法1块");
 
-		Node[] ans = tarJanQuery(head, qs);
+		int size = 1000;
+		int ques = 10000000;
+		System.out.println("节点个数 : " + size + ", 查询语句条数 : " + ques);
 
-		for (int i = 0; i != ans.length; i++) {
-			System.out.println("o1 : " + qs[i].o1.value);
-			System.out.println("o2 : " + qs[i].o2.value);
-			System.out.println("ancestor : " + ans[i].value);
-			System.out.println("===============");
-		}
+		int[] tree = generateTreeArray(size);
+		int[][] queries = generateQueries(ques, size);
+		long start;
+		long end;
+
+		start = System.currentTimeMillis();
+		query1(tree, queries);
+		end = System.currentTimeMillis();
+		System.out.println("方法1运行时间(毫秒) : " + (end - start));
+
+		start = System.currentTimeMillis();
+		query2(tree, queries);
+		end = System.currentTimeMillis();
+		System.out.println("方法2运行时间(毫秒) : " + (end - start));
+
+		System.out.println("性能测试结束");
 
 	}
+
 }
