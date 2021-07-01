21e8bd7e46b0d57c13cde55e7e1aa258a01b3d3a
algorithmzuo <algorithmzuo@163.com>
2021-07-01 18:06:40 +0800

    modify code

--- src/class21/Code01_TreeChainPartition.java
+++ src/class21/Code01_TreeChainPartition.java
@@ -146,16 +146,12 @@ public class Code01_TreeChainPartition {
 		private int[] arr;
 		private int[] sum;
 		private int[] lazy;
-		private int[] change;
-		private boolean[] update;
 
 		public SegmentTree(int[] origin) {
 			MAXN = origin.length;
 			arr = origin;
 			sum = new int[MAXN << 2];
 			lazy = new int[MAXN << 2];
-			change = new int[MAXN << 2];
-			update = new boolean[MAXN << 2];
 		}
 
 		private void pushUp(int rt) {
@@ -163,17 +159,6 @@ public class Code01_TreeChainPartition {
 		}
 
 		private void pushDown(int rt, int ln, int rn) {
-			if (update[rt]) {
-				update[rt << 1] = true;
-				update[rt << 1 | 1] = true;
-				change[rt << 1] = change[rt];
-				change[rt << 1 | 1] = change[rt];
-				lazy[rt << 1] = 0;
-				lazy[rt << 1 | 1] = 0;
-				sum[rt << 1] = change[rt] * ln;
-				sum[rt << 1 | 1] = change[rt] * rn;
-				update[rt] = false;
-			}
 			if (lazy[rt] != 0) {
 				lazy[rt << 1] += lazy[rt];
 				sum[rt << 1] += lazy[rt] * ln;
@@ -191,25 +176,6 @@ public class Code01_TreeChainPartition {
 			int mid = (l + r) >> 1;
 			build(l, mid, rt << 1);
 			build(mid + 1, r, rt << 1 | 1);
-			pushUp(rt);
-		}
-
-		public void update(int L, int R, int C, int l, int r, int rt) {
-			if (L <= l && r <= R) {
-				update[rt] = true;
-				change[rt] = C;
-				sum[rt] = C * (r - l + 1);
-				lazy[rt] = 0;
-				return;
-			}
-			int mid = (l + r) >> 1;
-			pushDown(rt, mid - l + 1, r - mid);
-			if (L <= mid) {
-				update(L, R, C, l, mid, rt << 1);
-			}
-			if (R > mid) {
-				update(L, R, C, mid + 1, r, rt << 1 | 1);
-			}
 			pushUp(rt);
 		}
 
