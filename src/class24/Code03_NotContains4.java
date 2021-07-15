994be0207bc9424edd6130e148489f2427a03f02
algorithmzuo <algorithmzuo@163.com>
2021-07-15 18:59:10 +0800

    modify code

--- src/class24/Code03_NotContains4.java
+++ src/class24/Code03_NotContains4.java
@@ -1,11 +1,12 @@
 package class24;
 
-// 里程表不能含有4，给定一个数max，返回他是里程表里的第几个
+// 里程表不能含有4，给定一个数num，返回他是里程表里的第几个
 public class Code03_NotContains4 {
 
-	public static long notContains4Nums1(long max) {
+	// num中一定没有4这个数字
+	public static long notContains4Nums1(long num) {
 		long count = 0;
-		for (long i = 1; i <= max; i++) {
+		for (long i = 1; i <= num; i++) {
 			if (isNot4(i)) {
 				count++;
 			}
@@ -23,60 +24,53 @@ public class Code03_NotContains4 {
 		return true;
 	}
 
-	public static long[] arr1 = { 0L, 8L, 72L, 648L, 5832L, 52488L, 472392L, 4251528L, 38263752L, 344373768L,
-			3099363912L, 27894275208L, 251048476872L, 2259436291848L, 20334926626632L, 183014339639688L,
-			1647129056757192L, 14824161510814728L, 133417453597332552L, 1200757082375992968L };
-
-	public static long[] arr2 = { 1L, 9L, 81L, 729L, 6561L, 59049L, 531441L, 4782969L, 43046721L, 387420489L,
+	// arr[1] : 比1位数还少1位，有几个数(数字里可以包含0但是不可以包含4)？0个
+	// arr[2] : 比2位数还少1位，有几个数(数字里可以包含0但是不可以包含4)？9个
+	// 1 -> 0 1 2 3 - 5 6 7 8 9 = 9
+	// arr[3] :比3位数还少1位，有几个数(数字里可以包含0但是不可以包含4)？81个
+	// 1 : 0 (0 1 2 3 - 5 6 7 8 9) = 9
+	// 2 :
+	// 1 (0 1 2 3 - 5 6 7 8 9) = 9
+	// 2 (0 1 2 3 - 5 6 7 8 9) = 9
+	// 3 (0 1 2 3 - 5 6 7 8 9) = 9
+	// 5 (0 1 2 3 - 5 6 7 8 9) = 9
+	// ...
+	// 9 (0 1 2 3 - 5 6 7 8 9) = 9
+	public static long[] arr = { 0L, 1L, 9L, 81L, 729L, 6561L, 59049L, 531441L, 4782969L, 43046721L, 387420489L,
 			3486784401L, 31381059609L, 282429536481L, 2541865828329L, 22876792454961L, 205891132094649L,
 			1853020188851841L, 16677181699666569L, 150094635296999121L, 1350851717672992089L };
 
-	public static long notContains4Nums2(long max) {
-		if (max <= 0) {
+	// num中一定没有4这个数字
+	public static long notContains4Nums2(long num) {
+		if (num <= 0) {
 			return 0;
 		}
-		int bits = bits(max);
-		long counts = 0;
-		for (int i = 1; i < bits; i++) {
-			counts += arr1[i];
-		}
-		long offset = offset(max);
-		long first = max / offset;
-		if (first == 4) {
-			counts += (first - 1) * arr2[bits - 1];
-		} else {
-			counts += process(max % offset, offset / 10, bits - 1)
-					+ (first < 4 ? (first - 1) : (first - 2)) * arr2[bits - 1];
-		}
-		return counts;
+		long offset = offset(num);
+		long first = num / offset;
+		int bits = bits(num);
+		return arr[bits] - 1 + (first - (first < 4 ? 1 : 2)) * arr[bits] + process(num % offset, offset / 10, bits - 1);
 	}
 
-	public static long process(long max, long offset, int bits) {
+	public static long process(long num, long offset, int bits) {
 		if (bits == 0) {
 			return 1;
 		}
-		long first = max / offset;
-		long counts = 0;
-		if (first == 4) {
-			counts += first * arr2[bits - 1];
-		} else {
-			counts += process(max % offset, offset / 10, bits - 1) + (first < 4 ? first : (first - 1)) * arr2[bits - 1];
-		}
-		return counts;
+		long first = num / offset;
+		return (first < 4 ? first : (first - 1)) * arr[bits] + process(num % offset, offset / 10, bits - 1);
 	}
 
-	public static int bits(long max) {
+	public static int bits(long num) {
 		int bits = 0;
-		while (max != 0) {
+		while (num != 0) {
 			bits++;
-			max /= 10;
+			num /= 10;
 		}
 		return bits;
 	}
 
-	public static long offset(long max) {
+	public static long offset(long num) {
 		long offset = 1;
-		while (max / offset > 9) {
+		while (num / offset > 9) {
 			offset *= 10;
 		}
 		return offset;
@@ -86,23 +80,24 @@ public class Code03_NotContains4 {
 		long max = 8888L;
 		System.out.println("功能测试开始，验证 0 ~ " + max + " 以内所有的结果");
 		for (long i = 0; i <= max; i++) {
-			if (notContains4Nums1(i) != notContains4Nums2(i)) {
+			// 测试的时候，输入的数字i里不能含有4，这是题目的规定！
+			if (isNot4(i) && notContains4Nums1(i) != notContains4Nums2(i)) {
 				System.out.println("Oops!");
 			}
 		}
 		System.out.println("如果没有打印Oops说明验证通过");
 
-		max = 888888888L;
+		long num = 392360215L;
 		long start;
 		long end;
-		System.out.println("性能测试开始，计算max = " + max + " 的答案");
+		System.out.println("性能测试开始，计算num = " + num + " 的答案");
 		start = System.currentTimeMillis();
-		long ans1 = notContains4Nums1(max);
+		long ans1 = notContains4Nums1(num);
 		end = System.currentTimeMillis();
 		System.out.println("方法一答案 : " + ans1 + ", 运行时间 : " + (end - start) + " ms");
 
 		start = System.currentTimeMillis();
-		long ans2 = notContains4Nums2(max);
+		long ans2 = notContains4Nums2(num);
 		end = System.currentTimeMillis();
 		System.out.println("方法二答案 : " + ans2 + ", 运行时间 : " + (end - start) + " ms");
 	}
