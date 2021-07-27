1e7508b2369b66899d907d73258638b00615efbb
algorithmzuo <algorithmzuo@163.com>
2021-07-27 19:31:32 +0800

    modify code

--- src/class27/Problem_0034_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/class27/Problem_0034_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -3,48 +3,28 @@ package class27;
 public class Problem_0034_FindFirstAndLastPositionOfElementInSortedArray {
 
 	public static int[] searchRange(int[] nums, int target) {
-		int[] ans = { -1, -1 };
 		if (nums == null || nums.length == 0) {
-			return ans;
+			return new int[] { -1, -1 };
 		}
-		ans[0] = findFirst(nums, target);
-		ans[1] = findLast(nums, target);
-		return ans;
+		int L = lessMostRight(nums, target) + 1;
+		if (L == nums.length || nums[L] != target) {
+			return new int[] { -1, -1 };
+		}
+		return new int[] { L, lessMostRight(nums, target + 1) };
 	}
 
-	public static int findFirst(int[] arr, int num) {
+	public static int lessMostRight(int[] arr, int num) {
 		int L = 0;
 		int R = arr.length - 1;
+		int M = 0;
 		int ans = -1;
-		int mid = 0;
 		while (L <= R) {
-			mid = L + ((R - L) >> 1);
-			if (arr[mid] < num) {
-				L = mid + 1;
-			} else if (arr[mid] > num) {
-				R = mid - 1;
+			M = L + ((R - L) >> 1);
+			if (arr[M] < num) {
+				ans = M;
+				L = M + 1;
 			} else {
-				ans = mid;
-				R = mid - 1;
-			}
-		}
-		return ans;
-	}
-
-	public static int findLast(int[] arr, int num) {
-		int L = 0;
-		int R = arr.length - 1;
-		int ans = -1;
-		int mid = 0;
-		while (L <= R) {
-			mid = L + ((R - L) >> 1);
-			if (arr[mid] < num) {
-				L = mid + 1;
-			} else if (arr[mid] > num) {
-				R = mid - 1;
-			} else {
-				ans = mid;
-				L = mid + 1;
+				R = M - 1;
 			}
 		}
 		return ans;
