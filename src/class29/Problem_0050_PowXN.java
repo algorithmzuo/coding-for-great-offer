07e6a313219e8cfa842e8318962131ea612e620c
algorithmzuo <algorithmzuo@163.com>
2021-08-03 19:44:00 +0800

    modify code

--- src/class29/Problem_0050_PowXN.java
+++ src/class29/Problem_0050_PowXN.java
@@ -2,27 +2,7 @@ package class29;
 
 public class Problem_0050_PowXN {
 
-	public static double myPow1(double x, int n) {
-		if (n == 0) {
-			return 1D;
-		}
-		if (n == Integer.MIN_VALUE) {
-			return (x == 1D || x == -1D) ? 1D : 0;
-		}
-		int pow = Math.abs(n);
-		double t = x;
-		double ans = 1D;
-		while (pow != 0) {
-			if ((pow & 1) != 0) {
-				ans *= t;
-			}
-			pow >>= 1;
-			t = t * t;
-		}
-		return n < 0 ? (1D / ans) : ans;
-	}
-
-	public static double myPow2(double x, int n) {
+	public static double myPow(double x, int n) {
 		if (n == 0) {
 			return 1D;
 		}
@@ -40,23 +20,6 @@ public class Problem_0050_PowXN {
 			ans *= x;
 		}
 		return n < 0 ? (1D / ans) : ans;
-	}
-
-	public static void main(String[] args) {
-		System.out.println("world shut up!");
-		int a = Integer.MIN_VALUE;
-		int b = -a;
-		System.out.println(b);
-
-		System.out.println("==============");
-
-		double test = 1.00000001D;
-		int N = Integer.MIN_VALUE;
-		System.out.println(test == 1D);
-		System.out.println(test + "的" + N + "次方，结果：");
-		System.out.println(Math.pow(test, (double) N));
-		System.out.println(myPow1(test, N));
-		System.out.println(myPow2(test, N));
 	}
 
 }
---
+++ src/class29/Problem_0639_DecodeWaysII.java
@@ -0,0 +1,99 @@
+package class29;
+
+public class Problem_0639_DecodeWaysII {
+
+	public static long mod = 1000000007;
+
+	public static int numDecodings1(String str) {
+		long[] dp = new long[str.length()];
+		return (int) ways1(str.toCharArray(), 0, dp);
+	}
+
+	public static long ways1(char[] s, int i, long[] dp) {
+		if (i == s.length) {
+			return 1;
+		}
+		if (s[i] == '0') {
+			return 0;
+		}
+		if (dp[i] != 0) {
+			return dp[i];
+		}
+		long ans = ways1(s, i + 1, dp) * (s[i] == '*' ? 9 : 1);
+		if (s[i] == '1' || s[i] == '2' || s[i] == '*') {
+			if (i + 1 < s.length) {
+				if (s[i + 1] == '*') {
+					ans += ways1(s, i + 2, dp) * (s[i] == '*' ? 15 : (s[i] == '1' ? 9 : 6));
+				} else {
+					if (s[i] == '*') {
+						ans += ways1(s, i + 2, dp) * (s[i + 1] < '7' ? 2 : 1);
+					} else {
+						ans += ((s[i] - '0') * 10 + s[i + 1] - '0') < 27 ? ways1(s, i + 2, dp) : 0;
+					}
+				}
+			}
+		}
+		ans %= mod;
+		dp[i] = ans;
+		return ans;
+	}
+
+	public static int numDecodings2(String str) {
+		char[] s = str.toCharArray();
+		int n = s.length;
+		long[] dp = new long[n + 1];
+		dp[n] = 1;
+		for (int i = n - 1; i >= 0; i--) {
+			if (s[i] != '0') {
+				dp[i] = dp[i + 1] * (s[i] == '*' ? 9 : 1);
+				if (s[i] == '1' || s[i] == '2' || s[i] == '*') {
+					if (i + 1 < n) {
+						if (s[i + 1] == '*') {
+							dp[i] += dp[i + 2] * (s[i] == '*' ? 15 : (s[i] == '1' ? 9 : 6));
+						} else {
+							if (s[i] == '*') {
+								dp[i] += dp[i + 2] * (s[i + 1] < '7' ? 2 : 1);
+							} else {
+								dp[i] += ((s[i] - '0') * 10 + s[i + 1] - '0') < 27 ? dp[i + 2] : 0;
+							}
+						}
+					}
+				}
+				dp[i] %= mod;
+			}
+		}
+		return (int) dp[0];
+	}
+
+	public static int numDecodings3(String str) {
+		char[] s = str.toCharArray();
+		int n = s.length;
+		long a = 1;
+		long b = 1;
+		long c = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			if (s[i] != '0') {
+				c = b * (s[i] == '*' ? 9 : 1);
+				if (s[i] == '1' || s[i] == '2' || s[i] == '*') {
+					if (i + 1 < n) {
+						if (s[i + 1] == '*') {
+							c += a * (s[i] == '*' ? 15 : (s[i] == '1' ? 9 : 6));
+						} else {
+							if (s[i] == '*') {
+								c += a * (s[i + 1] < '7' ? 2 : 1);
+							} else {
+								c += a * (((s[i] - '0') * 10 + s[i + 1] - '0') < 27 ? 1 : 0);
+							}
+						}
+					}
+				}
+			}
+			c %= mod;
+			a = b;
+			b = c;
+			c = 0;
+		}
+		return (int) b;
+	}
+
+}
