ecb604ae9749d3767ee7bf2305e235c860731713
algorithmzuo <algorithmzuo@163.com>
2021-08-05 11:16:45 +0800

    modify code

--- src/class30/Problem_0116_PopulatingNextRightPointersInEachNode.java
+++ src/class30/Problem_0116_PopulatingNextRightPointersInEachNode.java
@@ -2,11 +2,40 @@ package class30;
 
 public class Problem_0116_PopulatingNextRightPointersInEachNode {
 
+	// 不要提交这个类
 	public static class Node {
 		public int val;
 		public Node left;
 		public Node right;
 		public Node next;
+	}
+
+	// 提交下面的代码
+	public static Node connect(Node root) {
+		if (root == null) {
+			return root;
+		}
+		MyQueue queue = new MyQueue();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			// 第一个弹出的节点
+			Node pre = null;
+			int size = queue.size;
+			for (int i = 0; i < size; i++) {
+				Node cur = queue.poll();
+				if (cur.left != null) {
+					queue.offer(cur.left);
+				}
+				if (cur.right != null) {
+					queue.offer(cur.right);
+				}
+				if (pre != null) {
+					pre.next = cur;
+				}
+				pre = cur;
+			}
+		}
+		return root;
 	}
 
 	public static class MyQueue {
@@ -43,33 +72,6 @@ public class Problem_0116_PopulatingNextRightPointersInEachNode {
 			return ans;
 		}
 
-	}
-
-	public static Node connect(Node root) {
-		if (root == null) {
-			return root;
-		}
-		MyQueue queue = new MyQueue();
-		queue.offer(root);
-		while (!queue.isEmpty()) {
-			// 第一个弹出的节点
-			Node pre = null;
-			int size = queue.size;
-			for (int i = 0; i < size; i++) {
-				Node cur = queue.poll();
-				if (cur.left != null) {
-					queue.offer(cur.left);
-				}
-				if (cur.right != null) {
-					queue.offer(cur.right);
-				}
-				if (pre != null) {
-					pre.next = cur;
-				}
-				pre = cur;
-			}
-		}
-		return root;
 	}
 
 }
