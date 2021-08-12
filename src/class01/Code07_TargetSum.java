8a11067fdd7cf37e4fdbd87c9ab09b4694446b3c
algorithmzuo <algorithmzuo@163.com>
2021-08-12 15:02:29 +0800

    modify code

--- src/class01/Code07_TargetSum.java
+++ src/class01/Code07_TargetSum.java
@@ -53,8 +53,7 @@ public class Code07_TargetSum {
 	// 如果arr都是非负数，并且所有数的累加和是sum
 	// 那么如果target<sum，很明显没有任何方法可以达到target，可以直接返回0
 	// 优化点三 :
-	// 因为题目要求一定要使用所有数字去拼target，
-	// 所以不管这些数字怎么用+和-折腾，最终的结果都一定不会改变奇偶性
+	// arr内部的数组，不管怎么+和-，最终的结果都一定不会改变奇偶性
 	// 所以，如果所有数的累加和是sum，
 	// 并且与target的奇偶性不一样，没有任何方法可以达到target，可以直接返回0
 	// 优化点四 :
@@ -70,19 +69,45 @@ public class Code07_TargetSum {
 	// 也就是说，任何一个集合，只要累加和是(target + 数组所有数的累加和) / 2
 	// 那么就一定对应一种target的方式
 	// 也就是说，比如非负数组arr，target = 7, 而所有数累加和是11
-	// 求使用所有数字的情况下，多少方法最后形成7？
-	// 其实就是求有多少个子集的累加和是9 -> (7 + 11) / 2
+	// 求有多少方法组成7，其实就是求有多少种达到累加和(7+11)/2=9的方法
 	// 优化点五 :
 	// 二维动态规划的空间压缩技巧
-	public static int findTargetSumWays3(int[] arr, int target) {
+	public static int findTargetSumWays(int[] arr, int target) {
 		int sum = 0;
 		for (int n : arr) {
 			sum += n;
 		}
-		return sum < target || ((target & 1) ^ (sum & 1)) != 0 ? 0 : subset(arr, (target + sum) >> 1);
+		return sum < target || ((target & 1) ^ (sum & 1)) != 0 ? 0 : subset2(arr, (target + sum) >> 1);
 	}
 
-	public static int subset(int[] nums, int s) {
+	// 求nums有多少个子集，累加和是s
+	// 二维动态规划
+	// 不用空间压缩
+	public static int subset1(int[] nums, int s) {
+		int n = nums.length;
+		// dp[i][j] : nums前缀长度为i的所有子集，有多少累加和是j？
+		int[][] dp = new int[n + 1][s + 1];
+		// nums前缀长度为0的所有子集，有多少累加和是0？一个：空集
+		dp[0][0] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j <= s; j++) {
+				dp[i][j] = dp[i - 1][j];
+				if (j - nums[i - 1] >= 0) {
+					dp[i][j] += dp[i - 1][j - nums[i - 1]];
+				}
+			}
+		}
+		return dp[n][s];
+	}
+
+	// 求nums有多少个子集，累加和是s
+	// 二维动态规划
+	// 用空间压缩:
+	// 核心就是for循环里面的：for (int i = s; i >= n; i--) {
+	// 为啥不枚举所有可能的累加和？只枚举 n...s 这些累加和？
+	// 因为如果 i - n < 0，dp[i]怎么更新？和上一步的dp[i]一样！所以不用更新
+	// 如果 i - n >= 0，dp[i]怎么更新？上一步的dp[i] + 上一步dp[i - n]的值，这才需要更新
+	public static int subset2(int[] nums, int s) {
 		int[] dp = new int[s + 1];
 		dp[0] = 1;
 		for (int n : nums) {
