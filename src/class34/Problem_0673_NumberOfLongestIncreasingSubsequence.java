445bf8e3e6de1a76e597b101505a2f2f6fcecd86
algorithmzuo <algorithmzuo@163.com>
2021-08-17 17:12:36 +0800

    modify code

--- src/class34/Problem_0673_NumberOfLongestIncreasingSubsequence.java
+++ src/class34/Problem_0673_NumberOfLongestIncreasingSubsequence.java
@@ -10,35 +10,34 @@ public class Problem_0673_NumberOfLongestIncreasingSubsequence {
 			return 0;
 		}
 		ArrayList<TreeMap<Integer, Integer>> dp = new ArrayList<>();
-		for (int cur : nums) {
+		for (int num : nums) {
 			int L = 0;
 			int R = dp.size() - 1;
-			int find = -1;
+			int find = dp.size();
 			while (L <= R) {
 				int mid = (L + R) / 2;
-				if (dp.get(mid).firstKey() >= cur) {
+				if (dp.get(mid).firstKey() >= num) {
 					find = mid;
 					R = mid - 1;
 				} else {
 					L = mid + 1;
 				}
 			}
-			int num = 1;
-			int index = find == -1 ? dp.size() : find;
-			if (index > 0) {
-				TreeMap<Integer, Integer> lastMap = dp.get(index - 1);
-				num = lastMap.get(lastMap.firstKey());
-				if (lastMap.ceilingKey(cur) != null) {
-					num -= lastMap.get(lastMap.ceilingKey(cur));
+			int count = 1;
+			if (find > 0) {
+				TreeMap<Integer, Integer> lastMap = dp.get(find - 1);
+				count = lastMap.get(lastMap.firstKey());
+				if (lastMap.ceilingKey(num) != null) {
+					count -= lastMap.get(lastMap.ceilingKey(num));
 				}
 			}
-			if (index == dp.size()) {
+			if (find == dp.size()) {
 				TreeMap<Integer, Integer> newMap = new TreeMap<Integer, Integer>();
-				newMap.put(cur, num);
+				newMap.put(num, count);
 				dp.add(newMap);
 			} else {
-				TreeMap<Integer, Integer> curMap = dp.get(index);
-				curMap.put(cur, curMap.get(curMap.firstKey()) + num);
+				TreeMap<Integer, Integer> curMap = dp.get(find);
+				curMap.put(num, curMap.get(curMap.firstKey()) + count);
 			}
 		}
 		return dp.get(dp.size() - 1).firstEntry().getValue();
