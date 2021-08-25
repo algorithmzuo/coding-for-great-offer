865bca891837296381a295d06d501d0f9204aaf2
algorithmzuo <algorithmzuo@163.com>
2021-08-25 12:44:33 +0800

    modify code

--- src/class36/Code04_ComputeExpressionValue.java
+++ src/class36/Code04_ComputeExpressionValue.java
@@ -4,29 +4,37 @@ package class36;
 public class Code04_ComputeExpressionValue {
 
 	public static int sores(String s) {
-		return compute(s.toCharArray(), 0)[0] - 1;
+		return compute(s.toCharArray(), 0)[0];
 	}
 
 	public static int[] compute(char[] s, int i) {
 		if (s[i] == ')') {
-			return new int[] { 2, i };
+			return new int[] { 1, i };
 		}
 		int ans = 1;
 		while (i < s.length && s[i] != ')') {
 			int[] info = compute(s, i + 1);
-			ans *= info[0];
+			ans *= info[0] + 1;
 			i = info[1] + 1;
 		}
-		return new int[] { ans + 1, i };
+		return new int[] { ans, i };
 	}
 
 	public static void main(String[] args) {
+		// (()()) + (((()))) + ((())())
 		// (()()) -> 2 * 2 + 1 -> 5
 		// (((()))) -> 5
-		// ( (()) () ) -> (3 * 2) + 1 -> 7
+		// ((())()) -> ((2 + 1) * 2) + 1 -> 7
 		// 所以下面的结果应该是175
-		String str = "(()())(((())))((())())";
-		System.out.println(sores(str));
+		String str1 = "(()())(((())))((())())";
+		System.out.println(sores(str1));
+
+		// (()()()) + (()(()))
+		// (()()()) -> 2 * 2 * 2 + 1 -> 9
+		// (()(())) -> 2 * 3 + 1 -> 7
+		// 所以下面的结果应该是63
+		String str2 = "(()()())(()(()))";
+		System.out.println(sores(str2));
 	}
 
 }
