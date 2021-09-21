092a3262b4872866f9c5562347bbf2a93a87b94d
algorithmzuo <algorithmzuo@163.com>
2021-09-21 18:39:47 +0800

    modify code

--- src/class41/Problem_0265_PaintHouseII.java
+++ src/class41/Problem_0265_PaintHouseII.java
@@ -11,44 +11,44 @@ public class Problem_0265_PaintHouseII {
 			return 0;
 		}
 		int K = costs[0].length;
-		int allMin1 = 0;
-		int allEnd1 = -1;
-		int allMin2 = 0;
-		int allEnd2 = -1;
+		int preMin1 = 0;
+		int preEnd1 = -1;
+		int preMin2 = 0;
+		int preEnd2 = -1;
 		for (int i = 0; i < N; i++) {
 			int curMin1 = Integer.MAX_VALUE;
 			int curEnd1 = -1;
 			int curMin2 = Integer.MAX_VALUE;
 			int curEnd2 = -1;
 			for (int j = 0; j < K; j++) {
-				if (j != allEnd1) {
-					if (allMin1 + costs[i][j] <= curMin1) {
+				if (j != preEnd1) {
+					if (preMin1 + costs[i][j] < curMin1) {
 						curMin2 = curMin1;
 						curEnd2 = curEnd1;
-						curMin1 = allMin1 + costs[i][j];
+						curMin1 = preMin1 + costs[i][j];
 						curEnd1 = j;
-					} else if (allMin1 + costs[i][j] < curMin2) {
-						curMin2 = allMin1 + costs[i][j];
+					} else if (preMin1 + costs[i][j] < curMin2) {
+						curMin2 = preMin1 + costs[i][j];
 						curEnd2 = j;
 					}
-				} else if (j != allEnd2) {
-					if (allMin2 + costs[i][j] <= curMin1) {
+				} else if (j != preEnd2) {
+					if (preMin2 + costs[i][j] < curMin1) {
 						curMin2 = curMin1;
 						curEnd2 = curEnd1;
-						curMin1 = allMin2 + costs[i][j];
+						curMin1 = preMin2 + costs[i][j];
 						curEnd1 = j;
-					} else if (allMin2 + costs[i][j] < curMin2) {
-						curMin2 = allMin2 + costs[i][j];
+					} else if (preMin2 + costs[i][j] < curMin2) {
+						curMin2 = preMin2 + costs[i][j];
 						curEnd2 = j;
 					}
 				}
 			}
-			allMin1 = curMin1;
-			allEnd1 = curEnd1;
-			allMin2 = curMin2;
-			allEnd2 = curEnd2;
+			preMin1 = curMin1;
+			preEnd1 = curEnd1;
+			preMin2 = curMin2;
+			preEnd2 = curEnd2;
 		}
-		return allMin1;
+		return preMin1;
 	}
 
 }
