60655831d329b14f9379e481e78526f7429e0bd4
algorithmzuo <algorithmzuo@163.com>
2021-09-23 19:13:04 +0800

    modify code

--- src/class42/Problem_0272_ClosestBinarySearchTreeValueII.java
+++ src/class42/Problem_0272_ClosestBinarySearchTreeValueII.java
@@ -19,38 +19,38 @@ public class Problem_0272_ClosestBinarySearchTreeValueII {
 	// 这个解法来自讨论区的回答，最优解实现的很易懂且漂亮
 	public static List<Integer> closestKValues(TreeNode root, double target, int k) {
 		List<Integer> ret = new LinkedList<>();
-		Stack<TreeNode> backUpRight = new Stack<>();
-		Stack<TreeNode> backUpLeft = new Stack<>();
-		goLeft(root, target, backUpRight);
-		goRight(root, target, backUpLeft);
-		if (!backUpRight.isEmpty() && !backUpLeft.isEmpty() && backUpRight.peek().val == backUpLeft.peek().val) {
-			getPredecessor(backUpLeft);
+		Stack<TreeNode> moreTops = new Stack<>();
+		Stack<TreeNode> lessTops = new Stack<>();
+		getMoreTops(root, target, moreTops);
+		getLessTops(root, target, lessTops);
+		if (!moreTops.isEmpty() && !lessTops.isEmpty() && moreTops.peek().val == lessTops.peek().val) {
+			getPredecessor(lessTops);
 		}
 		while (k-- > 0) {
-			if (backUpRight.isEmpty()) {
-				ret.add(getPredecessor(backUpLeft));
-			} else if (backUpLeft.isEmpty()) {
-				ret.add(getSuccessor(backUpRight));
+			if (moreTops.isEmpty()) {
+				ret.add(getPredecessor(lessTops));
+			} else if (lessTops.isEmpty()) {
+				ret.add(getSuccessor(moreTops));
 			} else {
-				double diffs = Math.abs((double) backUpRight.peek().val - target);
-				double diffp = Math.abs((double) backUpLeft.peek().val - target);
+				double diffs = Math.abs((double) moreTops.peek().val - target);
+				double diffp = Math.abs((double) lessTops.peek().val - target);
 				if (diffs < diffp) {
-					ret.add(getSuccessor(backUpRight));
+					ret.add(getSuccessor(moreTops));
 				} else {
-					ret.add(getPredecessor(backUpLeft));
+					ret.add(getPredecessor(lessTops));
 				}
 			}
 		}
 		return ret;
 	}
 
-	public static void goLeft(TreeNode root, double target, Stack<TreeNode> backUpRight) {
+	public static void getMoreTops(TreeNode root, double target, Stack<TreeNode> moreTops) {
 		while (root != null) {
 			if (root.val == target) {
-				backUpRight.push(root);
+				moreTops.push(root);
 				break;
 			} else if (root.val > target) {
-				backUpRight.push(root);
+				moreTops.push(root);
 				root = root.left;
 			} else {
 				root = root.right;
@@ -58,13 +58,13 @@ public class Problem_0272_ClosestBinarySearchTreeValueII {
 		}
 	}
 
-	public static void goRight(TreeNode root, double target, Stack<TreeNode> backUpLeft) {
+	public static void getLessTops(TreeNode root, double target, Stack<TreeNode> lessTops) {
 		while (root != null) {
 			if (root.val == target) {
-				backUpLeft.push(root);
+				lessTops.push(root);
 				break;
 			} else if (root.val < target) {
-				backUpLeft.push(root);
+				lessTops.push(root);
 				root = root.right;
 			} else {
 				root = root.left;
@@ -72,23 +72,23 @@ public class Problem_0272_ClosestBinarySearchTreeValueII {
 		}
 	}
 
-	public static int getSuccessor(Stack<TreeNode> backUpRight) {
-		TreeNode cur = backUpRight.pop();
+	public static int getSuccessor(Stack<TreeNode> moreTops) {
+		TreeNode cur = moreTops.pop();
 		int ret = cur.val;
 		cur = cur.right;
 		while (cur != null) {
-			backUpRight.push(cur);
+			moreTops.push(cur);
 			cur = cur.left;
 		}
 		return ret;
 	}
 
-	public static int getPredecessor(Stack<TreeNode> backUpLeft) {
-		TreeNode cur = backUpLeft.pop();
+	public static int getPredecessor(Stack<TreeNode> lesstTops) {
+		TreeNode cur = lesstTops.pop();
 		int ret = cur.val;
 		cur = cur.left;
 		while (cur != null) {
-			backUpLeft.push(cur);
+			lesstTops.push(cur);
 			cur = cur.right;
 		}
 		return ret;
