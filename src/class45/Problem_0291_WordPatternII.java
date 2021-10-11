d88260e08b858cf67ce90ea7d97e16ed3cb4cc86
algorithmzuo <algorithmzuo@163.com>
2021-10-11 18:43:27 +0800

    modify code

--- src/class45/Problem_0291_WordPatternII.java
+++ src/class45/Problem_0291_WordPatternII.java
@@ -7,26 +7,25 @@ public class Problem_0291_WordPatternII {
 	public static boolean wordPatternMatch(String pattern, String str) {
 		String[] map = new String[26];
 		HashSet<String> set = new HashSet<>();
-		return process(str, pattern, 0, str.length() - 1, 0, pattern.length() - 1, map, set);
+		return process(str, pattern, 0, 0, map, set);
 	}
 
-	public static boolean process(String s, String p, int si, int se, int pi, int pe, String[] map,
-			HashSet<String> set) {
-		if (pi == pe + 1 && si == se + 1) {
+	public static boolean process(String s, String p, int si, int pi, String[] map, HashSet<String> set) {
+		if (pi == p.length() && si == s.length()) {
 			return true;
 		}
-		if ((pi > pe && si <= se) || (pi < pe && si > se)) {
+		if (pi == p.length() || si == s.length()) {
 			return false;
 		}
 		char ch = p.charAt(pi);
 		String matched = map[ch - 'a'];
 		if (matched != null) {
 			int count = matched.length();
-			return si + count <= se + 1 && matched.equals(s.substring(si, si + count))
-					&& process(s, p, si + matched.length(), se, pi + 1, pe, map, set);
+			return si + count <= s.length() && matched.equals(s.substring(si, si + count))
+					&& process(s, p, si + matched.length(), pi + 1, map, set);
 		} else {
-			int endPoint = se;
-			for (int i = pe; i > pi; i--) {
+			int endPoint = s.length() - 1;
+			for (int i = p.length() - 1; i > pi; i--) {
 				endPoint -= map[p.charAt(i) - 'a'] == null ? 1 : map[p.charAt(i) - 'a'].length();
 			}
 			for (int i = si; i <= endPoint; i++) {
@@ -34,7 +33,7 @@ public class Problem_0291_WordPatternII {
 				if (!set.contains(matched)) {
 					set.add(matched);
 					map[ch - 'a'] = matched;
-					if (process(s, p, i + 1, se, pi + 1, pe, map, set)) {
+					if (process(s, p, i + 1, pi + 1, map, set)) {
 						return true;
 					} else {
 						map[ch - 'a'] = null;
