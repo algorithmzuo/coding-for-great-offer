83cc0fcf6b9cd1cd21454f9e31d0aa0086702273
algorithmzuo <algorithmzuo@163.com>
2021-10-18 15:03:53 +0800

    modify code

--- src/class47/Problem_0475_Heaters.java
+++ src/class47/Problem_0475_Heaters.java
@@ -52,4 +52,61 @@ public class Problem_0475_Heaters {
 		return j == heaters.length - 1 || Math.abs(heaters[j] - houses[i]) < Math.abs(heaters[j + 1] - houses[i]);
 	}
 
+	// 下面这个方法不对，你能找出原因嘛？^_^
+	public static int findRadius2(int[] houses, int[] heaters) {
+		Arrays.sort(houses);
+		Arrays.sort(heaters);
+		int ans = 0;
+		// 时间复杂度O(N)
+		// i是地点，j是供暖点
+		for (int i = 0, j = 0; i < houses.length; i++) {
+			while (!best2(houses, heaters, i, j)) {
+				j++;
+			}
+			ans = Math.max(ans, Math.abs(heaters[j] - houses[i]));
+		}
+		return ans;
+	}
+
+	public static boolean best2(int[] houses, int[] heaters, int i, int j) {
+		return j == heaters.length - 1 || Math.abs(heaters[j] - houses[i]) <= Math.abs(heaters[j + 1] - houses[i]);
+	}
+
+	// 为了测试
+	public static int[] randomArray(int len, int v) {
+		int[] arr = new int[len];
+		for (int i = 0; i < len; i++) {
+			arr[i] = (int) (Math.random() * v) + 1;
+		}
+		return arr;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int len = 5;
+		int v = 10;
+		int testTime = 10000;
+		for (int i = 0; i < testTime; i++) {
+			int[] a = randomArray(len, v);
+			int[] b = randomArray(len, v);
+			int ans1 = findRadius(a, b);
+			int ans2 = findRadius2(a, b);
+			if (ans1 != ans2) {
+				System.out.println("A : ");
+				for (int num : a) {
+					System.out.print(num + " ");
+				}
+				System.out.println();
+				System.out.println("B : ");
+				for (int num : b) {
+					System.out.print(num + " ");
+				}
+				System.out.println();
+				System.out.println(ans1);
+				System.out.println(ans2);
+				break;
+			}
+		}
+	}
+
 }
