742832a4ff47551de9a194889b10830c08c0c715
algorithmzuo <algorithmzuo@163.com>
2021-11-15 12:10:17 +0800

    modify code

--- src/class24/Code05_MinWindowLength.java
+++ src/class24/Code05_MinWindowLength.java
@@ -8,14 +8,14 @@ public class Code05_MinWindowLength {
 		}
 		char[] str1 = s1.toCharArray();
 		char[] str2 = s2.toCharArray();
-		int[] map = new int[256]; // map[37] = 4  37  4次
+		int[] map = new int[256]; // map[37] = 4 37 4次
 		for (int i = 0; i != str2.length; i++) {
 			map[str2[i]]++;
 		}
 		int all = str2.length;
-		
-		// [L,R-1]  R
-		// [L,R)  -> [0,0)
+
+		// [L,R-1] R
+		// [L,R) -> [0,0)
 		int L = 0;
 		int R = 0;
 		int minLen = Integer.MAX_VALUE;
@@ -38,11 +38,43 @@ public class Code05_MinWindowLength {
 		return minLen == Integer.MAX_VALUE ? 0 : minLen;
 	}
 
-	public static void main(String[] args) {
-		String str1 = "adabbca";
-		String str2 = "acb";
-		System.out.println(minLength(str1, str2));
-
+	// 测试链接 : https://leetcode.com/problems/minimum-window-substring/
+	public static String minWindow(String s, String t) {
+		if (s.length() < t.length()) {
+			return "";
+		}
+		char[] str = s.toCharArray();
+		char[] target = t.toCharArray();
+		int[] map = new int[256];
+		for (char cha : target) {
+			map[cha]++;
+		}
+		int all = target.length;
+		int L = 0;
+		int R = 0;
+		int minLen = Integer.MAX_VALUE;
+		int ansl = -1;
+		int ansr = -1;
+		while (R != str.length) {
+			map[str[R]]--;
+			if (map[str[R]] >= 0) {
+				all--;
+			}
+			if (all == 0) {
+				while (map[str[L]] < 0) {
+					map[str[L++]]++;
+				}
+				if (minLen > R - L + 1) {
+					minLen = R - L + 1;
+					ansl = L;
+					ansr = R;
+				}
+				all++;
+				map[str[L++]]++;
+			}
+			R++;
+		}
+		return minLen == Integer.MAX_VALUE ? "" : s.substring(ansl, ansr + 1);
 	}
 
 }
