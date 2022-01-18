bf899188812a99b094057ea776c95995198a45a6
algorithmzuo <algorithmzuo@163.com>
2022-01-18 14:20:53 +0800

    modify code

--- src/class07/Code03_MaxGap.java
+++ src/class07/Code03_MaxGap.java
@@ -1,10 +1,9 @@
 package class07;
 
-import java.util.Arrays;
-
+// 测试链接 : https://leetcode.com/problems/maximum-gap/
 public class Code03_MaxGap {
 
-	public static int maxGap(int[] nums) {
+	public static int maximumGap(int[] nums) {
 		if (nums == null || nums.length < 2) {
 			return 0;
 		}
@@ -18,11 +17,10 @@ public class Code03_MaxGap {
 		if (min == max) {
 			return 0;
 		}
-		// 不止一种数，min~max一定有range,  len个数据，准备len+1个桶
-		boolean[] hasNum = new boolean[len + 1]; // hasNum[i] i号桶是否进来过数字
-		int[] maxs = new int[len + 1];  // maxs[i] i号桶收集的所有数字的最大值
-		int[] mins = new int[len + 1];  // mins[i] i号桶收集的所有数字的最小值
-		int bid = 0; // 桶号
+		boolean[] hasNum = new boolean[len + 1];
+		int[] maxs = new int[len + 1];
+		int[] mins = new int[len + 1];
+		int bid = 0;
 		for (int i = 0; i < len; i++) {
 			bid = bucket(nums[i], len, min, max);
 			mins[bid] = hasNum[bid] ? Math.min(mins[bid], nums[i]) : nums[i];
@@ -30,7 +28,7 @@ public class Code03_MaxGap {
 			hasNum[bid] = true;
 		}
 		int res = 0;
-		int lastMax = maxs[0]; // 上一个非空桶的最大值
+		int lastMax = maxs[0];
 		int i = 1;
 		for (; i <= len; i++) {
 			if (hasNum[i]) {
@@ -41,61 +39,8 @@ public class Code03_MaxGap {
 		return res;
 	}
 
-	// 如果当前的数是num，整个范围是min~max，分成了len + 1份
-	// 返回num该进第几号桶
 	public static int bucket(long num, long len, long min, long max) {
 		return (int) ((num - min) * len / (max - min));
-	}
-
-	// for test
-	public static int comparator(int[] nums) {
-		if (nums == null || nums.length < 2) {
-			return 0;
-		}
-		Arrays.sort(nums);
-		int gap = Integer.MIN_VALUE;
-		for (int i = 1; i < nums.length; i++) {
-			gap = Math.max(nums[i] - nums[i - 1], gap);
-		}
-		return gap;
-	}
-
-	// for test
-	public static int[] generateRandomArray(int maxSize, int maxValue) {
-		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-		}
-		return arr;
-	}
-
-	// for test
-	public static int[] copyArray(int[] arr) {
-		if (arr == null) {
-			return null;
-		}
-		int[] res = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			res[i] = arr[i];
-		}
-		return res;
-	}
-
-	// for test
-	public static void main(String[] args) {
-		int testTime = 500000;
-		int maxSize = 100;
-		int maxValue = 100;
-		boolean succeed = true;
-		for (int i = 0; i < testTime; i++) {
-			int[] arr1 = generateRandomArray(maxSize, maxValue);
-			int[] arr2 = copyArray(arr1);
-			if (maxGap(arr1) != comparator(arr2)) {
-				succeed = false;
-				break;
-			}
-		}
-		System.out.println(succeed ? "Nice!" : "Fucking fucked!");
 	}
 
 }
