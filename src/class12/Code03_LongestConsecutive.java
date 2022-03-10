6962f1ed7ef21837da839c063e72239e61bf2cc7
algorithmzuo <algorithmzuo@163.com>
2022-03-10 11:52:10 +0800

    modify code

--- src/class12/Code03_LongestConsecutive.java
+++ src/class12/Code03_LongestConsecutive.java
@@ -1,10 +1,12 @@
 package class12;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 // 本题测试链接 : https://leetcode.com/problems/longest-consecutive-sequence/
 public class Code03_LongestConsecutive {
 
+	// 课上讲的解法
 	public static int longestConsecutive(int[] nums) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		int len = 0;
@@ -20,6 +22,44 @@ public class Code03_LongestConsecutive {
 			}
 		}
 		return len;
+	}
+
+	// 补充一个两张表：头表、尾表。非常好理解的方法
+	// 不是最优解，但是好理解
+	public static int longestConsecutive2(int[] nums) {
+		HashMap<Integer, Integer> headMap = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> tailMap = new HashMap<Integer, Integer>();
+		HashSet<Integer> visited = new HashSet<>();
+		for (int num : nums) {
+			if (!visited.contains(num)) {
+				visited.add(num);
+				headMap.put(num, 1);
+				tailMap.put(num, 1);
+				if (tailMap.containsKey(num - 1)) {
+					int preLen = tailMap.get(num - 1);
+					int preHead = num - preLen;
+					headMap.put(preHead, preLen + 1);
+					tailMap.put(num, preLen + 1);
+					headMap.remove(num);
+					tailMap.remove(num - 1);
+				}
+				if (headMap.containsKey(num + 1)) {
+					int preLen = tailMap.get(num);
+					int preHead = num - preLen + 1;
+					int postLen = headMap.get(num + 1);
+					int postTail = num + postLen;
+					headMap.put(preHead, preLen + postLen);
+					tailMap.put(postTail, preLen + postLen);
+					headMap.remove(num + 1);
+					tailMap.remove(num);
+				}
+			}
+		}
+		int ans = 0;
+		for (int len : headMap.values()) {
+			ans = Math.max(ans, len);
+		}
+		return ans;
 	}
 
 }
