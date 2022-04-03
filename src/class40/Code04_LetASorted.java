f83371b78b1ba0796c1d6e806c1da5bc16d04f0f
algorithmzuo <algorithmzuo@163.com>
2022-04-03 10:36:03 +0800

    modify code

--- src/class40/Code04_LetASorted.java
+++ src/class40/Code04_LetASorted.java
@@ -5,47 +5,50 @@ package class40;
 // 你的目的是让A有序，返回你能不能做到
 public class Code04_LetASorted {
 
-	public static boolean letASorted(int[] A, int[] B) {
-		return process(A, B, 0, Integer.MIN_VALUE);
+	public static boolean letASorted1(int[] A, int[] B) {
+		return process1(A, B, 0, Integer.MIN_VALUE);
 	}
 
 	// 当前推进到了i位置，对于A和B都是i位置
 	// A[i]前一个数字，lastA
 	// 能否通过题意中的操作，A[i] B[i] 让A整体有序
-	public static boolean process(int[] A, int[] B, int i, int lastA) {
+	public static boolean process1(int[] A, int[] B, int i, int lastA) {
 		if (i == A.length) {
 			return true;
 		}
 		// 第一种选择 : A[i]不和B[i]交换
-		if (A[i] >= lastA && process(A, B, i + 1, A[i])) {
+		if (A[i] >= lastA && process1(A, B, i + 1, A[i])) {
 			return true;
 		}
-		// 第一种选择 : A[i]和B[i]交换
-		if (B[i] >= lastA && process(A, B, i + 1, B[i])) {
+		// 第二种选择 : A[i]和B[i]交换
+		if (B[i] >= lastA && process1(A, B, i + 1, B[i])) {
 			return true;
 		}
 		return false;
 	}
 
-	public static boolean process2(int[] A, int[] B, int i, int lastA) {
+	public static boolean letASorted2(int[] A, int[] B) {
+		return process2(A, B, 0, true);
+	}
+
+	// 当前推进到了i位置，对于A和B都是i位置
+	// A[i]前一个数字是否来自A ：
+	// 如果来自A，fromA = true；如果来自B，fromA = false；
+	// 能否通过题意中的操作，A[i] B[i] 让A整体有序
+	// 好处：可变参数成了int + boolean，时间复杂度可以做到O(N)
+	public static boolean process2(int[] A, int[] B, int i, boolean fromA) {
 		if (i == A.length) {
 			return true;
 		}
-		// 第一种选择 : A[i]不和B[i]交换
-		if (A[i] <= lastA && process2(A, B, i + 1, A[i])) {
+		if (i == 0 || (A[i] >= (fromA ? A[i - 1] : B[i - 1])) && process2(A, B, i + 1, true)) {
 			return true;
 		}
-		// 第一种选择 : A[i]和B[i]交换
-		if (B[i] <= lastA && process2(A, B, i + 1, B[i])) {
+		if (i == 0 || (B[i] >= (fromA ? A[i - 1] : B[i - 1])) && process2(A, B, i + 1, false)) {
 			return true;
 		}
 		return false;
 	}
 
-	// A B 操作 : A[i] 与 B[i] 交换！
-	// 目的 : 让A和B都有序，能不能做到
-//	public static boolean process3(int[] A, int[] B, int i, int lastA, int lastB) {
-//
-//	}
+	// 也可以彻底的贪心：就让A此时的值尽量小！也是可以的。时间复杂度O(N)
 
 }
