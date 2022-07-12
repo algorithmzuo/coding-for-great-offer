599128887a36c49afd2a263294c3c8f3a402a33d
algorithmzuo <algorithmzuo@163.com>
2022-07-12 15:49:20 +0800

    modify code

--- src/class14/Code04_CompleteTreeNodeNumber.java
+++ src/class14/Code04_CompleteTreeNodeNumber.java
@@ -1,19 +1,17 @@
 package class14;
 
+//本题测试链接 : https://leetcode.cn/problems/count-complete-tree-nodes/
 public class Code04_CompleteTreeNodeNumber {
 
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
-
-		public Node(int data) {
-			this.value = data;
-		}
+	// 提交时不要提交这个类
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 	}
 
-	// 请保证head为头的树，是完全二叉树
-	public static int nodeNum(Node head) {
+	// 提交如下的方法
+	public static int countNodes(TreeNode head) {
 		if (head == null) {
 			return 0;
 		}
@@ -22,7 +20,7 @@ public class Code04_CompleteTreeNodeNumber {
 
 	// 当前来到node节点，node节点在level层，总层数是h
 	// 返回node为头的子树(必是完全二叉树)，有多少个节点
-	public static int bs(Node node, int Level, int h) {
+	public static int bs(TreeNode node, int Level, int h) {
 		if (Level == h) {
 			return 1;
 		}
@@ -36,23 +34,12 @@ public class Code04_CompleteTreeNodeNumber {
 	// 如果node在第level层，
 	// 求以node为头的子树，最大深度是多少
 	// node为头的子树，一定是完全二叉树
-	public static int mostLeftLevel(Node node, int level) {
+	public static int mostLeftLevel(TreeNode node, int level) {
 		while (node != null) {
 			level++;
 			node = node.left;
 		}
 		return level - 1;
-	}
-
-	public static void main(String[] args) {
-		Node head = new Node(1);
-		head.left = new Node(2);
-		head.right = new Node(3);
-		head.left.left = new Node(4);
-		head.left.right = new Node(5);
-		head.right.left = new Node(6);
-		System.out.println(nodeNum(head));
-
 	}
 
 }
